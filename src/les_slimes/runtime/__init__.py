19d73a18160f84a1ffedab160ae4f84788691908
Jean-Philippe56520 <jeanphilippemaillet56@gmail.com>
2026-09-17 18:29:18 +0200

    feat: export h24 worker runtime

--- src/les_slimes/runtime/__init__.py
+++ src/les_slimes/runtime/__init__.py
@@ -1,19 +1,36 @@
 from .actors import ActorPermission, RuntimeActor
 from .canonical import AdvanceResult, CanonicalRuntime, RuntimeMetadata
 from .commands import apply_command, permission_for_command, validate_command_payload
-from .storage import RuntimeCommand, RuntimeStorage
+from .service import (
+    CanonicalWorkerService,
+    SystemClock,
+    WorkerHealth,
+    WorkerServiceConfig,
+)
+from .storage import (
+    RuntimeCommand,
+    RuntimeStorage,
+    WriterLease,
+    WriterLeaseLost,
+)
 from .worker import CanonicalWorldWorker, WorkerRunResult, WriterLeaseUnavailable
 
 __all__ = [
     "ActorPermission",
     "AdvanceResult",
     "CanonicalRuntime",
+    "CanonicalWorkerService",
     "CanonicalWorldWorker",
     "RuntimeActor",
     "RuntimeCommand",
     "RuntimeMetadata",
     "RuntimeStorage",
+    "SystemClock",
+    "WorkerHealth",
     "WorkerRunResult",
+    "WorkerServiceConfig",
+    "WriterLease",
+    "WriterLeaseLost",
     "WriterLeaseUnavailable",
     "apply_command",
     "permission_for_command",
