5f7669c1c8cdd1aa9faf80198d36e320d098608a
Jean-Philippe56520 <jeanphilippemaillet56@gmail.com>
2026-09-17 16:51:03 +0200

    feat: export canonical worker runtime

--- src/les_slimes/runtime/__init__.py
+++ src/les_slimes/runtime/__init__.py
@@ -1,3 +1,14 @@
 from .canonical import AdvanceResult, CanonicalRuntime, RuntimeMetadata
+from .storage import RuntimeCommand, RuntimeStorage
+from .worker import CanonicalWorldWorker, WorkerRunResult, WriterLeaseUnavailable
 
-__all__ = ["AdvanceResult", "CanonicalRuntime", "RuntimeMetadata"]
+__all__ = [
+    "AdvanceResult",
+    "CanonicalRuntime",
+    "CanonicalWorldWorker",
+    "RuntimeCommand",
+    "RuntimeMetadata",
+    "RuntimeStorage",
+    "WorkerRunResult",
+    "WriterLeaseUnavailable",
+]
