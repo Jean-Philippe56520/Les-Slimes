854fd8b1501679f9c69453d8ba51e005fcdc5760
Jean-Philippe56520 <jeanphilippemaillet56@gmail.com>
2026-09-17 17:45:23 +0200

    feat: export experiment fork API

--- src/les_slimes/experiments/__init__.py
+++ src/les_slimes/experiments/__init__.py
@@ -1,3 +1,16 @@
+from .forks import (
+    ExperimentManifest,
+    create_experiment_fork,
+    read_experiment_manifest,
+    run_experiment_fork,
+)
 from .runner import export_batch, run_batch
 
-__all__ = ["run_batch", "export_batch"]
+__all__ = [
+    "ExperimentManifest",
+    "create_experiment_fork",
+    "read_experiment_manifest",
+    "run_experiment_fork",
+    "run_batch",
+    "export_batch",
+]
